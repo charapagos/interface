8fdb26583a8f80ce346d78838cb65a77bfc17a7c
Tetsuji Asakawa <RXB01546@nifty.com>
2019-12-11 16:15:32 +0900

    add new cp_demo with glb

---
+++ cp_demo_v3_online/wordsTh.js
@@ -0,0 +1,10 @@
+var wordsTh = {
+  "ดีใจ": "glad",
+  "สุข": "happiness",
+  "ที่น่าสนใจ": "interest",
+  "ง่าย": "easiness",
+  "เศร้า": "sadness",
+  "โกรธ": "angriness",
+  "เจ็บปวด": "pain",
+  "เรื่องยาก": "difficulty",
+};
